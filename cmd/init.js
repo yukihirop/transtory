22f93cc5d5b4967b034611c68ccf1025ba66b647
yukihirop <te108186@gmail.com>
2020-01-03 23:05:35 +0900

    Create local repository: .trs

--- cmd/init.js
+++ cmd/init.js
@@ -1,5 +1,5 @@
 const { Command } = require('../src/monkey_patches/commander');
-const { yamlSafeLoad, yamlDumpWriteSyncFile } = require('../src/utils/file');
+const { yamlSafeLoad, yamlDumpWriteSyncFile, mkdirSyncRecursive, isExistFile, writeSyncFile } = require('../src/utils/file');
 
 const fs = require('fs')
   , currentPath = fs.realpathSync('./');
@@ -13,8 +13,22 @@ function InitCmd() {
     .action(() => {
       var yamlData = yamlSafeLoad('./src/template/transtory.yaml');
       var settingPath = `${currentPath}/.transtory`;
-      yamlDumpWriteSyncFile(settingPath, yamlData);
-      console.log(`created setting file: ${settingPath}`);
+
+      if (isExistFile(settingPath)) {
+        console.log(`already exist file: ${settingPath}`);
+      } else {
+        yamlDumpWriteSyncFile(settingPath, yamlData);
+        console.log(`created setting file: ${settingPath}`);
+      }
+
+      var trsPath = `${currentPath}/.trs`;
+      if (!isExistFile(trsPath)) {
+        mkdirSyncRecursive(`${trsPath}/objects`);
+        writeSyncFile(`${trsPath}/ADD`, '00000000000000000000000000000000');
+        writeSyncFile(`${trsPath}/HEAD`, '00000000000000000000000000000000');
+        writeSyncFile(`${trsPath}/FETCH`, '00000000000000000000000000000000');
+        writeSyncFile(`${trsPath}/COMMIT`, '00000000000000000000000000000000');
+      }
     });
 
   return initCmd
